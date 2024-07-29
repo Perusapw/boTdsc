76a35c7795e7bdc42ff3e921d700491d22773f3d
Perusapw <veraezter@gmail.com>
2024-07-29 16:10:41 +0700

    Update maintenance.js

--- src/commands/maintenance/maintenance.js
+++ src/commands/maintenance/maintenance.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js');
 
+module.exports = {
+    async execute(client, channel, server, time, reason) {
+        // Membuat pesan pengumuman maintenance
+        const message = `# MAINTENANCE\n\nServer: ${server}\nTime: ${time}\nReason: ${reason}`;
+
+        // Mengirim pengumuman maintenance ke channel yang ditentukan
+        if (channel && channel.isText()) {
+            await channel.send(message);
+            return { 
+                success: true,
+                message: `Maintenance announcement has been sent to ${channel}.`
+            };
+        } else {
+            return { 
+                success: false,
+                message: 'Selected channel is not valid.'
+            };
+        }
+    },
+};
+
+ 
